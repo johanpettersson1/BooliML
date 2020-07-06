4174ca233d64a7dd7687e472b667e4737e2fd523
Johan Pettersson <johanp7@kth.se>
2020-07-06 23:45:16 +0200

    init

---
+++ Booli/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Booli
+{
+    class Program
+    {
+        public static HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            string baseUrl = "https://api.booli.se/";
+            string callerId = ConfigurationManager.AppSettings["callerId"];
+            string privateKey = ConfigurationManager.AppSettings["privateKey"];
+            long timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            string unique = Get16CharacterRandomString();
+            string url = $"{baseUrl}listings?q=nacka&callerId={callerId}&time={timestamp}&unique={unique}&hash={Hash(callerId + timestamp + privateKey + unique)}";
+            using (Stream s = client.GetStreamAsync(url).Result)
+            using (StreamReader sr = new StreamReader(s))
+            {
+                string line = sr.ReadToEnd();
+                Console.WriteLine(line);
+                Console.ReadLine();
+            }
+        }
+
+        public static string Hash(string stringToHash)
+        {
+            using (var sha1 = new SHA1Managed())
+            {
+                return BitConverter.ToString(sha1.ComputeHash(Encoding.UTF8.GetBytes(stringToHash))).Replace("-", "");
+            }
+        }
+
+        public static string Get16CharacterRandomString()
+        {
+            return (Path.GetRandomFileName() + Path.GetRandomFileName()).Replace(".", "").Substring(0, 16);
+        }
+    }
+}
